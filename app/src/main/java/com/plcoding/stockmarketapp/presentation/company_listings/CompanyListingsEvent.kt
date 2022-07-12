1d18469a92b07d44d6fc03a25e9fcd34de93aaac
keith.ohare <keith.ohare@retailinmotion.com>
2022-07-12 21:12:55 +0100

    small fix

--- app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsEvent.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingsEvent.kt
@@ -1,6 +1,6 @@
 package com.plcoding.stockmarketapp.presentation.company_listings
 
 sealed interface CompanyListingsEvent {
-    object Refresh: CompanyListingsEvent()
+    object Refresh: CompanyListingsEvent
     data class OnSearchQueryChange(val query: String): CompanyListingsEvent
 }
